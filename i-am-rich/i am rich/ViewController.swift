3df71f22c45b6b16afd716ce460aa04f2c4d93d1
Felipe Mamede <felipebrasilmv@gmail.com>
2024-07-15 19:39:53 -0300

    :tada: first commit

---
+++ i-am-rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Felipe Mamede on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
